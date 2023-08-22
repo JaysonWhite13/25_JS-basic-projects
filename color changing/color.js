4cebcea0fb81848d6e124af8f6c6b9574b0f4d0d
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2023-08-22 03:26:59 +0100

    Update color.js

--- color changing/color.js
+++ color changing/color.js
@@ -2,7 +2,7 @@
 let i = 0;
 
 function changeColor(){
-  const arr = ['blue','yellow','red','orange','brown','green','pink','cyan','black',];
+  const arr = ['blue','yellow','red','orange','brown','green','pink','cyan','black'];
   let l = arr.length
   
   document.getElementsByTagName('div')[0].style.background = arr[i++];
