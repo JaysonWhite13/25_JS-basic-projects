ded4aacce1f81022c28512cad7c6cc2c2e62e234
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2023-08-22 01:34:11 +0100

    changing background color after clicking
    It's just sum javascript

---
+++ color changing/color.js
@@ -0,0 +1,17 @@
+
+function changeColor(){
+  var arr = ['red','blue','yellow','voilet','orange','brown'];
+  var l = arr.length;
+
+  for(i = 0;i < l;i++){
+    document.getElementsByTagName('div')[0].style.background = arr[i];
+
+  }
+ 
+ }
+
+
+
+
+ 
+ 
