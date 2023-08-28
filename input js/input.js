25f867632ca6bede6422391080cfc2feb1ab3923
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2023-08-28 15:16:15 +0100

    Update input.js

--- input js/input.js
+++ input js/input.js
@@ -1,7 +1,16 @@
+
+const text = document.getElementById('text');
+
+text.addEventListener("keydown", (event) => {
+    if(event.key === "Enter")
+      textcollector();
+  });//here is for it to function when the ENTER button is pressed
+
 function textcollector(){
 
-var text = document.querySelector('#text').value;
-
-document.getElementById('here').innerHTML = text;
+document.getElementById('here').innerHTML = text.value;
+text.value = "";
 
 }
+
+
