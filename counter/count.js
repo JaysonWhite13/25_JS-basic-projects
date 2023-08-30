acc8411fcb8ecb54075a19d085304fba261aec35
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2023-08-30 04:55:36 +0100

    Update count.js

--- counter/count.js
+++ counter/count.js
@@ -21,3 +21,17 @@ function decrease(){
 
 
 
+//shorter methods
+/* var take = document.getElementById('num').innerHTML;
+var num = Number(take);
+
+function increase(a){
+    var i;
+    for(i=0;i<100;i++){
+        num =  num + a; 
+        return document.getElementById('num').innerHTML = num;
+    }
+
+}*/
+
+
