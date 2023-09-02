fc42c9eeeb5c44758577f3e3f1e2d378353aed96
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2023-09-02 04:57:29 +0100

    Update clock.js

--- digital clock/clock.js
+++ digital clock/clock.js
@@ -1,8 +1,8 @@
 function searchDate(){
 var m = new Date();
+var hour =  m.getHours();
+var min = m.getMinutes();
 
-document.getElementById('hour').innerHTML = m.getHours();
-document.getElementById('min').innerHTML = m.getMinutes();
 
 if(m.getHours() > 12){
     document.getElementById('secs').innerHTML = "PM";
@@ -10,12 +10,20 @@ if(m.getHours() > 12){
     document.getElementById('secs').innerHTML = "AM";
 }
 
+if(hour < 10){
+    hour = "0"+hour
+}
+if(min < 10){
+    min = "0"+min
+}
+
+
 var days  = ["SUN"," MON","TUE","WED","THU","FRI","SAT"]
 var i = m.getDay() ;
 
+document.getElementById('min').innerHTML = min;
+document.getElementById('hour').innerHTML = hour;
 document.getElementById('day').innerHTML = days[i];
-
-
 setTimeout(searchDate, 200)
 
 }
