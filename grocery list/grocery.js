8ec75e974658957ba7b9e7a88092f6e15eb59719
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2023-09-11 14:31:54 +0100

    Update grocery.js

--- grocery list/grocery.js
+++ grocery list/grocery.js
@@ -1,9 +1,30 @@
-var input = document.getElementById('input').value;
+var input = document.querySelector('input');
+var list =  document.getElementById('here');
+var eraser =  document.getElementById('here');
 
-input.addEventListener("keypress", function(event) {
-    if (event.key === "space") {
-      event.preventDefault();
-      document.getElementById("myBtn").click();
+
+input.addEventListener("keypress", (e)=> {
+
+    if(e.key == "Enter" ){
+        addItems();
+
     }
+     
+} );
+function erase(){
+   eraser.innerHTML = "";
+}
 
-});
+
+function addItems(){
+    let h2 = document.createElement("h2");
+    h2.innerHTML = "-" + input.value;
+
+    h2.addEventListener("click" , () =>{
+        h2.style.textDecoration = "line-through"; 
+    })
+ 
+   list.insertAdjacentElement( "beforebegin",h2);
+
+   input.value = "";
+}
